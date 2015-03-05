2ad85cb7a01712d51a335373117c9c59d43f1932
Jonathan Stewart <jjstewart007@gmail.com>
2015-03-04 22:02:12 -0500

    Added Driver.py

---
+++ Driver.py
@@ -0,0 +1,3 @@
+from SheetReader import *
+
+sheetReader = SheetReader()
---
+++ SheetReader.py
@@ -0,0 +1,17 @@
+import xlrd
+
+class SheetReader:
+    
+    def __init__(self):
+        self.currentRow = 1
+
+    def getNextBatch(batchSize):
+        return 0
+
+    def setSheet(excelSheet):
+        self.sheet = excelSheet
+
+    def hasMoreRows():
+        if currentRow < excelSheet.nrows:
+            return true
+        return false
